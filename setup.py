da3f67344d8d22d6dc6621f18088124b2b2f3bd1
vahndi.minah <vahndi.minah@frogdesign.com>
2020-05-28 14:30:49 -0400

    v0.257

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
     name='mpl_format',
     packages=find_packages(),
-    version='0.256',
+    version='0.257',
     license='MIT',
     description='Library for easier formatting of matplotlib plots written in a functional style.',
     author='Vahndi Minah',
     url='https://github.com/vahndi/mpl-format',
-    download_url='https://github.com/vahndi/mpl-format/archive/v_0.256.tar.gz',
+    download_url='https://github.com/vahndi/mpl-format/archive/v_0.257.tar.gz',
     keywords=['matplotlib'],
     install_requires=[
         'matplotlib',
