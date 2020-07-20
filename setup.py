f8ebacd8fc4f9b9876748617ae45b06aac25afd5
vahndi.minah <vahndi.minah@frogdesign.com>
2020-07-20 19:30:03 -0400

    v0.270

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
     name='mpl_format',
     packages=find_packages(),
-    version='0.269',
+    version='0.270',
     license='MIT',
     description='Library for easier formatting of matplotlib plots written in '
                 'a functional style.',
