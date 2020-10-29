a1ecc37ae616629c2a1f318ebde2478212414848
vahndi.minah <vahndi.minah@frogdesign.com>
2020-10-29 16:05:51 -0400

    add styles file

---
+++ mpl_format/styles.py
@@ -0,0 +1,82 @@
+from enum import Enum
+
+
+class LINE_STYLE(Enum):
+
+    solid = 1
+    dashed = 2
+    dashdot = 3
+    dotted = 4
+
+
+class CAP_STYLE(Enum):
+
+    butt = 1
+    round = 2
+    projecting = 3
+
+
+class JOIN_STYLE(Enum):
+
+    miter = 1
+    round = 2
+    bevel = 3
+
+
+class ARROW_STYLE(Enum):
+
+    Curve = 1
+    CurveB = 2
+    BracketB = 3
+    CurveFilledB = 4
+    CurveA = 5
+    CurveAB = 6
+    CurveFilledA = 7
+    CurveFilledAB = 8
+    BracketA = 9
+    BracketAB = 10
+    Fancy = 11
+    Simple = 12
+    Wedge = 13
+    BarAB = 14
+
+    def get_name(self):
+
+        return {
+            'Curve': '-',
+            'CurveB': '->',
+            'BracketB': '-[',
+            'CurveFilledB': '-|>',
+            'CurveA': '<-',
+            'CurveAB': '<->',
+            'CurveFilledA': '<|-',
+            'CurveFilledAB': '<|-|>',
+            'BracketA': ']-',
+            'BracketAB': ']-[',
+            'Fancy': 'fancy',
+            'Simple': 'simple',
+            'Wedge': 'wedge',
+            'BarAB': '|-|',
+        }[self.name]
+
+
+class CONNECTION_STYLE(Enum):
+
+    angle = 1
+    angle3 = 2
+    arc = 3
+    arc3 = 4
+    bar = 5
+
+
+class BOX_STYLE(Enum):
+
+    circle = 1
+    darrow = 2
+    larrow = 3
+    rarrow = 4
+    round = 5
+    round4 = 6
+    roundtooth = 7
+    sawtooth = 8
+    square = 9
