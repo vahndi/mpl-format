86392869afc76fcd76f69444338226d1e2ab64c5
vahndi.minah <vahndi.minah@frogdesign.com>
2020-12-16 22:43:59 -0500

    add waves to ColorAnimation

--- mpl_format/animation/kwarg_animations/color_animation.py
+++ mpl_format/animation/kwarg_animations/color_animation.py
@@ -1,6 +1,6 @@
 from typing import List, Union, Optional
 
-from numpy import linspace
+from numpy import linspace, sin, pi, sign, arcsin
 
 from mpl_format.compound_types import Color
 from mpl_format.animation.rate import Rate
@@ -33,13 +33,83 @@ class ColorAnimation(object):
             t=[0, 1]
         )
 
+    @classmethod
+    def sine_wave(cls, color_1: Color, color_2: Color,
+                  cycles: float, phase: float = 0.0) -> 'ColorAnimation':
+        """
+        Return a sine wave oscillating between color_1 and color_2,
+        starting at phase * 2π with cycles complete waves.
+
+        :param color_1: Lowest color of the wave.
+        :param color_2: Highest color of the wave.
+        :param cycles: Number of complete oscillations.
+        :param phase: Value from 0 to 1 of where to start the wave.
+        """
+        middle_val = 0.5
+        amplitude = 0.5
+        rate = Rate(lambda t: (
+                middle_val +
+                amplitude * sin((2 * pi * t * cycles) - (2 * pi * phase))
+        ))
+        return ColorAnimation(colors=[color_1, color_2],
+                              rate=rate)
+
+    @classmethod
+    def square_wave(cls, color_1: Color, color_2: Color,
+                    cycles: float, phase: float = 0.0) -> 'FloatAnimation':
+        """
+        Return a square wave oscillating between min_val and max_val,
+        starting at phase * 2π with cycles complete waves.
+
+        :param color_1: Lowest color of the wave.
+        :param color_2: Highest color of the wave.
+        :param cycles: Number of complete oscillations.
+        :param phase: Value from 0 to 1 of where to start the wave.
+        """
+        middle_val = 0.5
+        amplitude = 0.5
+        rate = Rate(lambda t: (
+                middle_val +
+                amplitude * sign(
+                    sin((2 * pi * t * cycles) - (2 * pi * phase))
+                )
+        ))
+        return ColorAnimation(colors=[color_1, color_2],
+                              rate=rate)
+
+    @classmethod
+    def triangle_wave(cls, color_1: Color, color_2: Color,
+                      cycles: float, phase: float = 0.0) -> 'FloatAnimation':
+        """
+        Return a triangle wave oscillating between min_val and max_val,
+        starting at phase * 2π with cycles complete waves.
+
+        :param color_1: Lowest color of the wave.
+        :param color_2: Highest color of the wave.
+        :param cycles: Number of complete oscillations.
+        :param phase: Value from 0 to 1 of where to start the wave.
+        """
+        middle_val = 0.5
+        amplitude = 0.5
+        rate = Rate(lambda t: (
+                middle_val +
+                2 * amplitude * arcsin(
+                    sin((2 * pi * t * cycles) - (2 * pi * phase))
+                ) / pi
+        ))
+        return ColorAnimation(colors=[color_1, color_2],
+                              rate=rate)
+
     def at(self, t: float) -> Color:
 
         for i in range(len(self.t)):
             if self.t[i + 1] >= t:
+                dt = self.rate(
+                    (t - self.t[i]) /
+                    (self.t[i + 1] - self.t[i])
+                )
                 return cross_fade(
                     from_color=self.colors[i],
                     to_color=self.colors[i + 1],
-                    amount=((t - self.t[i]) /
-                            (self.t[i + 1] - self.t[i]))
+                    amount=dt
                 )
